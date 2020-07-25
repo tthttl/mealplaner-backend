13f05b9469fa4e2510c89f92833b255aa4fb1700
p18750 <david.lichtsteiner@css.ch>
2020-07-25 15:08:39 +0200

    Init: Remove Credentials from Code

--- config/database.js
+++ config/database.js
@@ -4,16 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.mvdgc.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'mealplaner'),
-        username: env('DATABASE_USERNAME', 'str4p1'),
-        password: env('DATABASE_PASSWORD', 'zqZ!J7M$'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: env.bool('DATABASE_SSL'),
       },
     },
   },
