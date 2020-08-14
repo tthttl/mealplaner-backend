35e0ad9931775ea48b51d4ae0cd075c9706ba178
p18750 <david.lichtsteiner@css.ch>
2020-08-14 10:13:48 +0200

    Add: Only check for id to match in isTargetUserLoggedIn.js

--- config/policies/isTargetUserLoggedIn.js
+++ config/policies/isTargetUserLoggedIn.js
@@ -1,17 +1,13 @@
 module.exports = async (ctx, next) => {
-  if(!ctx.state.user){
-    return ctx.unauthorized("Unauthorized")
-  }
+  const targetUserId = ctx.request.query.user;
 
-  const targetUser = ctx.request.query.owner || ctx.request.query.sharedWith;
-
-  if(!targetUser) {
+  if(!targetUserId) {
     return ctx.unauthorized("Specify a target user equal to your own id ?user=${user.id} or ?sharedWith=${user.id}")
   }
 
   const loggedInUser = ctx.state.user.id;
 
-  if (loggedInUser.toString() !== targetUser.toString()) {
+  if (loggedInUser.toString() !== targetUserId.toString()) {
     return ctx.unauthorized("Target user does not match logged in user");
   }
 
