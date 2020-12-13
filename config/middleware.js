df53a7d0e46de4e5d6a29c0eb53e05c860e37408
p19210 <attila.toth@css.ch>
2020-12-13 11:32:43 +0100

    add https address to cors

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,13 @@
 module.exports = {
   settings: {
     cors: {
-      origin: ['http://127.0.0.1:8080', "http://localhost:4200", "http://localhost:8080", "http://458df8543ed0.ngrok.io"]
+      origin: [
+        'http://127.0.0.1:8080',
+        "http://localhost:4200",
+        "http://localhost:8080",
+        "http://458df8543ed0.ngrok.io",
+        "https://458df8543ed0.ngrok.io"
+      ]
     },
   },
 };
